90857973a18c5afeacffa857788edbfb9477829e
Shubham Patrick <ahthneeuhl@gmail.com>
2024-04-18 13:30:09 +0530

    chore: express setup

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Welcome to India's first NodeJS server");
+import app from "./src/app";
+
+const startServer = () => {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+};
+
+startServer();
